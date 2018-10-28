835acd14e3ccc6ca7e6afb15c165a5d149bd023f
enya <essidanaya@gmail.com>
2018-10-28 20:23:04 +0300

    create entity

---
+++ src/Entity/Post.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PostRepository")
+ */
+class Post
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $autor;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $add_time;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getAutor(): ?string
+    {
+        return $this->autor;
+    }
+
+    public function setAutor(string $autor): self
+    {
+        $this->autor = $autor;
+
+        return $this;
+    }
+
+    public function getAddTime(): ?\DateTimeInterface
+    {
+        return $this->add_time;
+    }
+
+    public function setAddTime(\DateTimeInterface $add_time): self
+    {
+        $this->add_time = $add_time;
+
+        return $this;
+    }
+}
